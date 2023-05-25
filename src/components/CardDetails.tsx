08e874aea58b83a2f98324af4908f7a0ea8213c7
5iveemmy <ainemmanuel87@gmail.com>
2023-05-25 23:46:47 +0100

    chore: design the upper part of the card details

--- src/components/CardDetails.tsx
+++ src/components/CardDetails.tsx
@@ -1,14 +1,65 @@
 import { Box, Flex, Image, Text } from "@chakra-ui/react"
 import receipt from "../assets/receipt.png"
+import masterCard from "../assets/masterCardLogo.png"
+import chip from "../assets/chip.png"
+import { RiWifiFill, TbBrandApple, RxBorderDotted } from "react-icons/all"
 
 const CardDetails = () => {
   return (
-    <Box width="40%">
+    <Box width="30%">
+      <Flex
+        pt="40px"
+        pb="10px"
+        justifyContent="space-between"
+        bgColor="white"
+        width="260px"
+        m="0 auto"
+        borderTopRadius="20px"
+        boxShadow="-1px -16px 20px 20px #f6f6f6"
+        px="30px"
+      >
+        <Image
+          src={chip}
+          height="40px"
+          width="40px"
+          transform="rotate(90deg)"
+        />
+        <RiWifiFill fontSize="30px" />
+      </Flex>
       <Box borderRadius="20px" bgColor="#e7ecee" px="30px">
+        <Box
+          bgColor="white"
+          borderBottomRadius="20px"
+          p="30px"
+          pt="80px"
+          boxShadow="1px 20px 20px 0px #e4e4e4"
+        >
+          <Box>
+            <Text fontSize="16px">Jonathan Michael</Text>
+            <Flex alignItems="center" gap="10px">
+              <RxBorderDotted fontSize="50px" />
+              <Text fontWeight="700" fontSize="20px">
+                3456
+              </Text>
+            </Flex>
+          </Box>
+          <Flex justifyContent="space-between" alignItems="center">
+            <Text fontWeight="500">09/22</Text>
+            <Flex flexDir="column" alignItems="center" gap="4px" pt="30px">
+              <Image src={masterCard} height="30px" width="50px" />
+              <Text as="span" fontSize="8px">
+                mastercard
+              </Text>
+            </Flex>
+          </Flex>
+        </Box>
         <Box pt="40px">
           <Flex justifyContent="space-between">
             <Text opacity="0.5">Company</Text>
-            <Text fontWeight="bold">Apple</Text>
+            <Flex alignItems="center" gap="4px">
+              <TbBrandApple />
+              <Text fontWeight="bold">Apple</Text>
+            </Flex>
           </Flex>
           <Flex justifyContent="space-between" pt="10px">
             <Text opacity="0.5">Order Number</Text>
