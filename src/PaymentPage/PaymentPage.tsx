2a0f2e9dd1a96a8348dc38cfaea144fa50bbca00
5iveemmy <ainemmanuel87@gmail.com>
2023-05-25 20:04:48 +0100

    chore: import masterCard logo

--- src/PaymentPage/PaymentPage.tsx
+++ src/PaymentPage/PaymentPage.tsx
@@ -1,14 +1,30 @@
 import NumberBox from "@/components/NumberBox"
-import { Box, Flex, Text } from "@chakra-ui/react"
-
+import {
+  Box,
+  Button,
+  Flex,
+  Image,
+  Input,
+  InputGroup,
+  InputLeftAddon,
+  // InputRightAddon,
+  Text,
+} from "@chakra-ui/react"
+import { BsFillPenFill } from "react-icons/bs"
+import masterCard from "../assets/masterCardLogo.png"
 const PaymentPage = () => {
   return (
-    <Box pt="80px" px="70px">
-      <Flex width="500px" justifyContent="space-between">
-        <Flex>
+    <Box pt="80px" px="70px" width="700px">
+      <Flex justifyContent="space-between">
+        <Flex gap="10px">
+          <Box
+            bgColor="#025efe"
+            height="30px"
+            width="30px"
+            borderRadius="full"
+          />
           <Text fontSize="20px">
             <Text as="span" fontWeight="800">
-              {" "}
               AceCoin
             </Text>
             Pay
@@ -26,6 +42,68 @@ const PaymentPage = () => {
           </Flex>
         </Flex>
       </Flex>
+      <Box pt="30px">
+        <Flex justifyContent="space-between" alignItems="center">
+          <Box>
+            <Text fontWeight="700">Card Number</Text>
+            <Text color="#000000" fontSize="12px" pt="5px" opacity="0.5">
+              Enter the 16 digit card number on the card
+            </Text>
+          </Box>
+          <Flex gap="8px">
+            <BsFillPenFill color="#025efe" />
+            <Text color="#025efe" fontSize="12px" fontWeight="600">
+              Edit
+            </Text>
+          </Flex>
+        </Flex>
+      </Box>
+      <InputGroup mt="20px">
+        <InputLeftAddon height="50px" bgColor="#f9fbfc" borderRight="none">
+          <Image src={masterCard} height="15px" width="25px" />
+        </InputLeftAddon>
+        <Input
+          placeholder="2412  -  7512  -  3412  -  3456"
+          bgColor="#f9fbfc"
+          borderLeft="none"
+          borderRight="none"
+          borderColor="#f3f2f5"
+          height="50px"
+          _focusVisible={{ border: "none", boxShadow: "none" }}
+        />
+      </InputGroup>
+      <Flex justifyContent="space-between" mt="20px" alignItems="center">
+        <Box>
+          <Text fontWeight="700">CVV Number</Text>
+          <Text color="#565656" fontSize="10px" pt="5px">
+            Enter the 3 or 4 digit number on the card
+          </Text>
+        </Box>
+        <Input placeholder="327" width="50%" />
+      </Flex>
+      <Flex justifyContent="space-between" mt="20px" alignItems="center">
+        <Box>
+          <Text fontWeight="700">Expiry Date</Text>
+          <Text color="#565656" fontSize="10px" pt="5px">
+            Enter the expiration date of the card
+          </Text>
+        </Box>
+        <Flex gap="10px" alignItems="center" width="50%">
+          <Input placeholder="09" />
+          /
+          <Input placeholder="22" />
+        </Flex>
+      </Flex>
+      <Button
+        bgColor="#025efe"
+        color="white"
+        width="100%"
+        mt="20px"
+        borderRadius="8px"
+        height="45px"
+      >
+        Pay Now
+      </Button>
     </Box>
   )
 }
